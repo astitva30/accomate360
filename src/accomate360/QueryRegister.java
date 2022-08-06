bc76a0d12a6bafe9b73087858de6ba7fb1b4f994
Astitva <deshpandeastitva@gmail.com>
2022-08-06 11:47:15 +0530

    latest commit

--- src/accomate360/QueryRegister.java
+++ src/accomate360/QueryRegister.java
@@ -4,22 +4,27 @@ import emergency.connection.CallTheConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 
 public class QueryRegister {
-    private static int queryID;
+    
     public static void registerQuery(){
         
         try{
-            queryID++;
             String userId = JOptionPane.showInputDialog("Enter USER ID : ");
             String propertyId = JOptionPane.showInputDialog("Enter Property ID: ");
             String problem = JOptionPane.showInputDialog("Enter problem description: ");
             String status = "unsolved";
             
+            
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = CallTheConnection.connectionCode();
+//            PreparedStatement st1= con.prepareStatement("select * from querytable where queryId in (select MAX(queryId) queryId from querytable)");
             PreparedStatement st = con.prepareStatement("insert into querytable values(?,?,?,?,?)");
+//            ResultSet rs = st1.executeQuery();
+//            int queryID = rs.getInt(1);
+            int queryID=0;
             st.setInt(1, queryID);
             st.setString(2, userId);
             st.setString(3, propertyId);
@@ -31,5 +36,8 @@ public class QueryRegister {
         catch(ClassNotFoundException | SQLException ex){
             ex.printStackTrace();
         }
+    }
+    public static void showAll(){
+        
     }
 }
